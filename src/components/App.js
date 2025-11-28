0055b9506f8d189c34ea379a243b5067ad50af29
Akshay <mandaakshay20@gmail.com>
2025-11-28 12:09:20 +0530

    Wrote code according to the description

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <div id="main">
+      {/* Do not remove the main div */}
 
-export default App
+      <Tooltip text="This is a tooltip message">
+        <h2>Hover over me</h2>
+      </Tooltip>
+
+      <hr />
+
+      <Tooltip text="This is another tooltip example">
+        <p>Hover over me to see another tooltip</p>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default App;
